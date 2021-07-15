1b191c4e0d0dcd2e8436ed1291ec4588cf42e2b7
Pedro Gabriel <pedrogabriel12423@gmail.com>
2021-07-15 14:56:30 -0300

    Iniciando estudos sobre encapsulamento

---
+++ Aula06/src/Aula06/Aula06.java
@@ -0,0 +1,6 @@
+package Aula06;
+public class Aula06 {
+    public static void main(String[] args){
+
+    }
+}
---
+++ Aula06/src/Aula06/Controlador.java
@@ -0,0 +1,5 @@
+package Aula06;
+
+public interface Controlador {
+	public abstract void ligar();
+}
